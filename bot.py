5983c88ba8dd75f802371ae1b5847b05205493c5
HugoMoran3 <hugo.moran3@gmail.com>
2021-07-12 15:41:14 +0930

    Commands to connect to author channel

--- bot.py
+++ bot.py
@@ -1,16 +1,37 @@
 import discord
-from discord.ext import commands
-from discord import FFmpegPCMAudio
-import requests
-import json
+from discord.ext import commands, tasks
+from discord import FFmpegPCMAudio,ChannelType
+import datetime
+from asyncio import sleep
+import  time
 import os
 from dotenv import load_dotenv
+import asyncio
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = commands.Bot(command_prefix = "!",)
+
+channel_id = 862961024931725314
+
+# get localtime, create
+curr_time = time.localtime()
+curr_clock = time.strftime("The time is %H:%M:%S", curr_time)
+trigger = curr_time.tm_min == 00
+print(trigger)
+print(curr_clock)
+
+
+@client.command()
+async def channels(ctx):
+    voice_channel_list = ctx.guild.voice_channels
+    print(voice_channel_list[0])
+    channelname = voice_channel_list[0]
+    print(voice_channel_list)
+
 
 @client.event
 async def on_ready():
@@ -23,6 +44,8 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
+
+
 @client.command(pass_context = True)
 async def join(ctx):
     if (ctx.author.voice):
@@ -30,8 +53,13 @@ async def join(ctx):
         voice = await channel.connect()
         source = FFmpegPCMAudio('bigben.mp3')
         player = voice.play(source)
+        while voice.is_playing():
+            await sleep(1)
+        await voice.disconnect()
     else:
         await ctx.send("You are not in a voice channel, you must be in a voice channel to run this command.")
+
+
 
 @client.command(pass_context = True)
 async def leave(ctx): 
@@ -41,5 +69,21 @@ async def leave(ctx):
     else:
         await ctx.send("I am not in a voice channel")
 
+def timetracker():  
+    if trigger == True:
+        alarm()
+    else:
+        print("It's not time for big ben")
+
+@tasks.loop(seconds=1)
+async def alarm():
+    print("Joined channel")
+    channel = client.get_channel(channel_id)
+    join()
+    print("Joined channel")
+
+
 
 client.run(TOKEN)
+
+
